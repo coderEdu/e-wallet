ce62b3ac4502a6f148ffee1afd74afc7327af34c
coderEdu <educoder78@gmail.com>
2022-10-15 11:38:47 -0300

    add include files header & footer

--- home.php
+++ home.php
@@ -1,2 +1,15 @@
-<?php include ("conn.php"); ?>
-Bienvenido a tu billetera alectrónica.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Bienvenido</title>
+</head>
+<body>
+    <div class="container flex w-full h-screen bg-yellow-50 mx-auto">
+        <h1 class="flex flex-row text-2xl w-fit mx-auto content-center p-10 text-red-800">Bienvenido a tu billetera electrónica.</h1>
+    </div>
+</body>
+</html>
