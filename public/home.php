40939f9ae922dae6a966a3cf371a6f31ff387e94
coderEdu <educoder78@gmail.com>
2022-10-16 01:19:28 -0300

    crea header nav

--- public/home.php
+++ public/home.php
@@ -10,17 +10,22 @@
 </head>
 <body>
     <div class="container flex flex-col w-full h-screen bg-yellow-50 mx-auto">
-        <!-- mainmenu navbar -->
-        <div class="bg-yellow-400 w-full h-24">
-            <div class="">logo</div>
-            <div>
-                <nav>
-
-                </nav>
+        <nav class="flex flex-row bg-yellow-400 w-full h-24 justify-between">
+            <!-- logo -->
+            <div class="w-24 h-full mx-6 my-2">
+                <img src="img/atm-money-icon.png" alt="atm-money-icon.png">
             </div>
-        </div>
-
-        <h1 class="flex flex-row text-2xl w-fit mx-auto content-center p-10 text-red-800">Bienvenido a tu billetera electrónica.</h1>
+            <!-- mainmenu navbar -->
+            <div class="">
+                <ul class="flex flex-row w-fit h-full text-md p-6 space-x-4 items-center">
+                    <a href="#"><li>Extraer</li></a>
+                    <a href="#"><li>Depositar</li></a>
+                    <a href="#"><li>Registro</li></a>
+                    <a href="#"><li>Cerrar sesión</li></a>
+                </ul>
+            </div>
+        </nav>
+        <h1 class="flex flex-row text-2xl w-fit mx-auto content-center p-10 text-red-800">Bienvenido a tu Billetera Electrónica.</h1>
     </div>
 </body>
 </html>
