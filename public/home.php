9992279b00f376a05b706c094520bb97005462d8
coderEdu <educoder78@gmail.com>
2022-10-17 00:35:08 -0300

    create account list & las movs and notes

--- public/home.php
+++ public/home.php
@@ -12,20 +12,38 @@
     <div class="container flex flex-col w-full h-screen bg-yellow-50 mx-auto">
         <nav class="flex flex-row bg-yellow-400 w-full h-24 justify-between">
             <!-- logo -->
-            <div class="w-24 h-full mx-6 my-2">
+            <div class="w-1/4 sm:w-24 sm:h-full mx-6 my-2">
                 <img src="img/atm-money-icon.png" alt="atm-money-icon.png">
             </div>
             <!-- mainmenu navbar -->
-            <div class="">
+            <div class="hidden md:flex">
                 <ul class="flex flex-row w-fit h-full text-md p-6 space-x-4 items-center">
                     <a href="#"><li>Extraer</li></a>
                     <a href="#"><li>Depositar</li></a>
-                    <a href="#"><li>Registro</li></a>
+                    <a href="#"><li>Ver registro</li></a>
                     <a href="#"><li>Cerrar sesión</li></a>
                 </ul>
             </div>
         </nav>
+        
         <h1 class="flex flex-row text-2xl w-fit mx-auto content-center p-10 text-red-800">Bienvenido a tu Billetera Electrónica.</h1>
+
+        <!-- account list -->
+        <div class="flex flex-col mb-5 sm:flex-row sm:flex-wrap w-full content-between items-center bg-slate-200">
+            <div class="flex w-full h-48 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 bg-slate-400"></div>
+            <div class="flex w-full h-48 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 bg-slate-400"></div>
+            <div class="flex w-full h-48 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 bg-slate-400"></div>
+            <div class="flex w-full h-48 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 bg-slate-400"></div>
+            <div class="flex w-full h-48 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 bg-slate-400"></div>          
+        </div>
+
+        <div class="flex flex-col sm:flex-row sm:flex-wrap w-full content-between items-center bg-slate-200">
+            <!-- last movements -->
+            <div class="flex w-full h-64 sm:w-[47.5%] md:w-[47.8%] lg:w-[48.4%] xl:w-[48.7%] 2xl:w-[48.9%] m-2 bg-slate-400"></div>
+
+            <!-- last notes -->
+            <div class="flex w-full h-64 sm:w-[47.5%] md:w-[47.8%] lg:w-[48.4%] xl:w-[48.7%] 2xl:w-[48.9%] m-2 bg-slate-400"></div>
+        </div>
     </div>
 </body>
 </html>
