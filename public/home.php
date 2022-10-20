a8f1d8dd93de2a00ed54a5851547b8c5ff4f7f9b
coderEdu <educoder78@gmail.com>
2022-10-20 00:37:06 -0300

    card is almost there

--- public/home.php
+++ public/home.php
@@ -2,26 +2,25 @@
     <div class="container flex flex-col w-full h-screen mx-auto bg-default">
         <?php include "includes/header-nav.php"; ?>
         
-        <h1 class="flex flex-row text-lg sm:text-xl w-fit mx-auto content-center p-8 lg:p-10 text-slate-900">Bienvenido a tu Billetera Electrónica.</h1>
+        <h1 class="flex flex-row text-lg sm:text-xl w-fit mx-auto content-center p-8 lg:p-10 text-slate-900">Bienvenido/a a tu Billetera Electrónica.</h1>
 
         <!-- account list -->
         <div class="flex flex-col mb-5 sm:flex-row sm:flex-wrap w-full content-between items-center rounded-2xl">
-            <div class="flex w-full h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 rounded-2xl bg-slate-400">
-                <div>
+            <div class="flex w-full h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 py-4 px-6 rounded-2xl card-box-shadow card-box-shadow:hover bg-white">
+                <div class="flex flex-col w-[80%]">
+                    <p class="font-serif mb-1 text-md text-zinc-500">GOTECH</p>
+                    <h2 class="def-f-family font-medium text-2xl text-black"><i class="fa fa-dollar-sign"></i> 164589,00</i></h2>
                     <div>
-                        <p></p>
-                        <h2><i></i></h2>
-                        <div>
-                            <span>
-                                <i></i>
-                            </span>
-                            <span></span>
-                        </div>
+                        <span class="font-bold text-sm mr-1 text-green-700"><i class="fa fa-arrow-down"></i></span>
+                        <span class="text-sm text-zinc-500">12</span>
+                        <div class="inline-block w-4"></div>
+                        <span class="font-bold text-sm mr-1 text-red-700"><i class="fa fa-arrow-up"></i></span>
+                        <span class="text-sm text-zinc-500">34</span>
                     </div>
-                    <div>
-                        <div>
-                            <i></i>
-                        </div>
+                </div>
+                <div class="flex flex-col w-[20%]">
+                    <div class="bg-emerald-100 w-16 h-16 md:w-12 md:h-12 xl:w-16 xl:h-16 rounded-full">
+                        <i class="fa fa-suitcase"></i>
                     </div>
                 </div>
             </div>
--- public/includes/header.php
+++ public/includes/header.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+
     <link href="css/myStyles.css" rel="stylesheet">
     <link href="output.css" rel="stylesheet">
+
+    <!-- FontAwesome Kit -->
+    <script src="https://kit.fontawesome.com/cf3e3a7ec6.js" crossorigin="anonymous"></script>
     <title>Bienvenido</title>
 </head>
 <body class="bg-default">
