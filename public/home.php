b861196b80646d9cab4bbc1a96162d1032705a83
coderEdu <educoder78@gmail.com>
2022-10-27 18:57:36 -0300

    some important changes

--- public/home.php
+++ public/home.php
@@ -3,213 +3,19 @@
         <?php include "includes/header-nav.php"; ?>        
         
         <!-- account list -->
-        <div class="flex flex-col mb-5 pt-10 md:pt-12 lg:pt-14 sm:flex-row sm:flex-wrap w-full content-between items-center rounded-2xl">
-            <h1 class="flex font-sans text-lg sm:text-xl w-full mx-auto content-start px-3 py-4 text-slate-900">Tus cuentas</h1>
-            <div class="flex w-11/12 h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 py-4 px-6 rounded-2xl card-box-shadow card-box-shadow:hover bg-white"> <!-- card 1 -->
-                <div class="flex flex-col w-[80%]">
-                    <p class="font-serif mb-1 text-md text-zinc-500">GOTECH</p>
-                    <a href="#">
-                        <h2 class="def-f-family font-medium text-2xl text-black"><i class="fa fa-dollar-sign"></i> 164589,00</i></h2>
-                    </a>
-                    <div class="flex w-1/2 gap-2">
-                        <div>
-                            <span class="text-zinc-500">Movs:</span>
-                        </div>
-                        <div class="flex content-center items-center">
-                            <span class="font-bold text-sm mr-1 text-green-700"><i class="fa fa-arrow-down"></i></span>
-                            <span class="text-sm text-zinc-500">2</span>
-                        </div>
-                        <div class="flex content-center items-center">
-                            <span class="font-bold text-sm mr-1 text-red-700"><i class="fa fa-arrow-up"></i></span>
-                            <span class="text-sm text-zinc-500">8</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="flex flex-col w-[20%]">
-                    <div class="flex bg-emerald-100 w-16 h-16 md:w-12 md:h-12 xl:w-16 xl:h-16 rounded-full items-center">
-                        <i class="fa fa-wallet text-[1.5rem] md:text-[1.4rem] xl:text-[1.5rem] text-lime-700 mx-auto"></i>
-                    </div>
-                </div>
-            </div>
-            <div class="flex w-11/12 h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 py-4 px-6 rounded-2xl card-box-shadow card-box-shadow:hover bg-white"> <!-- card 2 -->
-                <div class="flex flex-col w-[80%]">
-                    <p class="font-serif mb-1 text-md text-zinc-500">DELMA</p>
-                    <a href="#">
-                        <h2 class="def-f-family font-medium text-2xl text-black"><i class="fa fa-dollar-sign"></i> 14149,80</i></h2>
-                    </a>
-                    <div class="flex w-1/2 gap-2">
-                        <div>
-                            <span class="text-zinc-500">Movs:</span>
-                        </div>
-                        <div class="flex content-center items-center">
-                            <span class="font-bold text-sm mr-1 text-green-700"><i class="fa fa-arrow-down"></i></span>
-                            <span class="text-sm text-zinc-500">105</span>
-                        </div>
-                        <div class="flex content-center items-center">
-                            <span class="font-bold text-sm mr-1 text-red-700"><i class="fa fa-arrow-up"></i></span>
-                            <span class="text-sm text-zinc-500">134</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="flex flex-col w-[20%]">
-                    <div class="flex bg-emerald-100 w-16 h-16 md:w-12 md:h-12 xl:w-16 xl:h-16 rounded-full items-center">
-                        <i class="fa fa-wallet text-[1.5rem] md:text-[1.4rem] xl:text-[1.5rem] text-lime-700 mx-auto"></i>
-                    </div>
-                </div>
-            </div>
-            <div class="flex w-11/12 h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 py-4 px-6 rounded-2xl card-box-shadow card-box-shadow:hover bg-white"> <!-- card 3 -->
-                <div class="flex flex-col w-[80%]">
-                    <p class="font-serif mb-1 text-md text-zinc-500">DESBLOQUEOS</p>
-                    <a href="#">
-                        <h2 class="def-f-family font-medium text-2xl text-black"><i class="fa fa-dollar-sign"></i> 4500,00</i></h2>
-                    </a>
-                    <div class="flex w-1/2 gap-2">
-                        <div>
-                            <span class="text-zinc-500">Movs:</span>
-                        </div>
-                        <div class="flex content-center items-center">
-                            <span class="font-bold text-sm mr-1 text-green-700"><i class="fa fa-arrow-down"></i></span>
-                            <span class="text-sm text-zinc-500">4</span>
-                        </div>
-                        <div class="flex content-center items-center">
-                            <span class="font-bold text-sm mr-1 text-red-700"><i class="fa fa-arrow-up"></i></span>
-                            <span class="text-sm text-zinc-500">30</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="flex flex-col w-[20%]">
-                    <div class="flex bg-yellow-100 w-16 h-16 md:w-12 md:h-12 xl:w-16 xl:h-16 rounded-full items-center">
-                        <i class="fa fa-wallet text-[1.5rem] md:text-[1.4rem] xl:text-[1.5rem] text-yellow-500 mx-auto"></i>
-                    </div>
-                </div>
-            </div>
-            <div class="flex w-11/12 h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 py-4 px-6 rounded-2xl card-box-shadow card-box-shadow:hover bg-white"> <!-- card 4 -->
-                <div class="flex flex-col w-[80%]">
-                    <p class="font-serif mb-1 text-md text-zinc-500">IFE 5</p>
-                    <a href="#">
-                        <h2 class="def-f-family font-medium text-2xl text-black"><i class="fa fa-dollar-sign"></i> 89,12</i></h2>
-                    </a>
-                    <div class="flex w-1/2 gap-2">
-                        <div>
-                            <span class="text-zinc-500">Movs:</span>
-                        </div>
-                        <div class="flex content-center items-center">
-                            <span class="font-bold text-sm mr-1 text-green-700"><i class="fa fa-arrow-down"></i></span>
-                            <span class="text-sm text-zinc-500">1</span>
-                        </div>
-                        <div class="flex content-center items-center">
-                            <span class="font-bold text-sm mr-1 text-red-700"><i class="fa fa-arrow-up"></i></span>
-                            <span class="text-sm text-zinc-500">340</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="flex flex-col w-[20%]">
-                    <div class="flex bg-red-100 w-16 h-16 md:w-12 md:h-12 xl:w-16 xl:h-16 rounded-full items-center">
-                        <i class="fa fa-wallet text-[1.5rem] md:text-[1.4rem] xl:text-[1.5rem] text-red-400 mx-auto"></i>
-                    </div>
-                </div>
-            </div>
-            <div class="flex w-11/12 h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 py-4 px-6 rounded-2xl card-box-shadow card-box-shadow:hover bg-white"> <!-- card 5 -->
-                <div class="flex flex-col w-[80%]">
-                    <p class="font-serif mb-1 text-md text-zinc-500">EDU</p>
-                    <a href="#">
-                        <h2 class="def-f-family font-medium text-2xl text-black"><i class="fa fa-dollar-sign"></i> 7164349,18</i></h2>
-                    </a>
-                    <div class="flex w-1/2 gap-2">
-                        <div>
-                            <span class="text-zinc-500">Movs:</span>
-                        </div>
-                        <div class="flex content-center items-center">
-                            <span class="font-bold text-sm mr-1 text-green-700"><i class="fa fa-arrow-down"></i></span>
-                            <span class="text-sm text-zinc-500">1200</span>
-                        </div>
-                        <div class="flex content-center items-center">
-                            <span class="font-bold text-sm mr-1 text-red-700"><i class="fa fa-arrow-up"></i></span>
-                            <span class="text-sm text-zinc-500">132340</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="flex flex-col w-[20%]">
-                    <div class="flex bg-blue-100 w-16 h-16 md:w-12 md:h-12 xl:w-16 xl:h-16 rounded-full items-center">
-                        <i class="fa fa-wallet text-[1.5rem] md:text-[1.4rem] xl:text-[1.5rem] text-blue-500 mx-auto"></i>
-                    </div>
-                </div>
-            </div>
-            <!--
-            <div class="flex w-full h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 rounded-2xl bg-slate-400"></div>
-            <div class="flex w-full h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 rounded-2xl bg-slate-400"></div>
-            <div class="flex w-full h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 rounded-2xl bg-slate-400"></div>
-            <div class="flex w-full h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 rounded-2xl bg-slate-400"></div>  
-            -->        
-        </div>
+        <?php /* include "includes/partials/home/account-list.php" */ ?>
 
         <div class="flex flex-col sm:flex-row sm:flex-wrap w-full content-between items-center rounded-2xl">
             <!-- last movements -->
-            <div class="flex flex-col w-11/12 h-auto sm:w-[47.5%] md:w-[47.8%] lg:w-[48.4%] xl:w-[48.7%] 2xl:w-[48.9%] m-2 rounded-2xl bg-slate-100">
-                <h1 class="flex font-sans text-lg sm:text-lg w-full mx-auto content-start px-3 py-4 text-slate-900">Tus últimos movimientos</h1>
+            <div class="flex flex-col w-11/12 h-auto sm:w-[47.5%] md:w-[47.8%] lg:w-[48.4%] xl:w-[48.7%] 2xl:w-[48.9%] m-2 rounded-2xl mov-card-b-shadow bg-white">
+                <div class="flex flex-row w-full ml-4 justify-center items-center">
+                    <span class="flex "><i class="fa fa-money-bill text-xl text-slate-500""></i></span>
+                    <h1 class="flex font-sans text-lg sm:text-lg w-full mx-auto content-start px-3 py-4  text-slate-700">Tus últimos movimientos</h1>
+                </div>
                 <div class="flex w-full h-0.5 border-b-2 border-gray-200"></div>
-                <!--
-                <table class="w-full text-center">
-                    <thead class="bg-black text-slate-50">
-                        <tr">
-                            <th>Fecha</th>
-                            <th>Tipo</th>
-                            <th>Monto</th>
-                            <th>Cuenta</th>
-                        </tr>
-                    </thead>
-                    <tr>
-                        <td>9/3/2022 1:26:04 AM</td>
-                        <td>Ext</td>
-                        <td>$400</td>
-                        <td>Gotech</td>
-                    </tr>
-                    <tr>
-                        <td>9/3/2022 1:24:58 AM</td>
-                        <td>Ext</td>
-                        <td>$400</td>
-                        <td>Delma</td>
-                    </tr>
-                    <tr>
-                        <td>8/8/2022 5:14:05 PM</td>
-                        <td>Dep</td>
-                        <td>$17000</td>
-                        <td>Edu</td>
-                    </tr>
-                    <tr>
-                        <td>21/8/2022 12:57:51 AM</td>
-                        <td>Ext</td>
-                        <td>$7000</td>
-                        <td>Desbloqueos</td>
-                    </tr>
-                    <tr>
-                        <td>8/8/2022 5:14:05 PM</td>
-                        <td>Dep</td>
-                        <td>$17000</td>
-                        <td>Edu</td>
-                    </tr>
-                    <tr>
-                        <td>21/8/2022 12:57:51 AM</td>
-                        <td>Ext</td>
-                        <td>$7000</td>
-                        <td>Desbloqueos</td>
-                    </tr>
-                    <tr>
-                        <td>8/8/2022 5:14:05 PM</td>
-                        <td>Dep</td>
-                        <td>$17000</td>
-                        <td>Edu</td>
-                    </tr>
-                    <tr>
-                        <td>21/8/2022 12:57:51 AM</td>
-                        <td>Ext</td>
-                        <td>$7000</td>
-                        <td>Desbloqueos</td>
-                    </tr>
-                </table>
-                -->
 
                 <!-- first movement card -->
+                <!--
                 <div class="flex w-full h-auto py-4 px-6 mb-1 card-box-shadow card-box-shadow:hover bg-white">
                     <div class="flex flex-col w-[80%] divide-y-2">
 
@@ -218,13 +24,13 @@
                             <h3 class="font-serif mb-1 text-md text-zinc-500">9 mar 2022 1:26:04</h3>
                         </div>
 
-                        <div class="flex flex-row-reverse w-full mt-2 justify-between">
-                            <a href="#">
-                                <h2 class="def-f-family font-medium text-2xl text-black"><i class="fa fa-dollar-sign"></i> 400,0</i></h2>
-                            </a>
+                        <div class="flex w-full pt-1 justify-between">
                             <div class="flex flex-wrap w-3/4 gap-2">
                                 <span class="text-zinc-600">Paddle con Andy, Sergio y Dampier en...</span>
                             </div>
+                            <a href="#">
+                                <h2 class="def-f-family font-medium text-2xl text-black"><i class="fa fa-dollar-sign"></i> 400,0</i></h2>
+                            </a>
                         </div>
 
                     </div>
@@ -235,8 +41,10 @@
                         <i class="flex text-red-700 fa fa-arrow-up"></i>
                     </div>
                 </div>
+                -->
 
                 <!-- second movement card -->
+                <!--
                 <div class="flex w-full h-auto py-4 px-6 mb-1 card-box-shadow card-box-shadow:hover bg-white"> 
                     <div class="flex flex-col w-[80%] divide-y-2">
 
@@ -245,7 +53,7 @@
                             <h3 class="font-serif mb-1 text-md text-zinc-500">13 oct 2022 14:21:44</h3>
                         </div>
 
-                        <div class="flex flex-row-reverse w-full mt-2 justify-between">
+                        <div class="flex flex-row-reverse w-full justify-between">
                             <a href="#">
                                 <h2 class="def-f-family font-medium text-2xl text-black"><i class="fa fa-dollar-sign"></i> 3342,68</i></h2>
                             </a>
@@ -262,17 +70,18 @@
                         <i class="flex text-red-700 fa fa-arrow-up"></i>
                     </div>
                 </div>
+                -->
 
-                 <!-- second movement card -->
+                 <!-- third movement card -->
                  <div class="flex w-full h-auto py-4 px-6 mb-1 card-box-shadow card-box-shadow:hover bg-white"> 
-                    <div class="flex flex-col w-[80%] divide-y-2">
+                    <div class="flex flex-col w-full divide-y-2">
 
                         <div class="flex w-full justify-between">
-                            <p class="font-serif mb-1 text-md text-zinc-500">GOTECH</p>
+                            <span class="flex font-serif mb-1 py-0 px-3 bg-green-200 rounded-xl text-md text-zinc-900">GOTECH</span>
                             <h3 class="font-serif mb-1 text-md text-zinc-500">24 oct 2022 17:12:04</h3>
                         </div>
 
-                        <div class="flex flex-row-reverse w-full mt-2 justify-between">
+                        <div class="flex flex-row-reverse w-full justify-between">
                             <a href="#">
                                 <h2 class="def-f-family font-medium text-2xl text-black"><i class="fa fa-dollar-sign"></i> 2500,00</i></h2>
                             </a>
@@ -282,12 +91,14 @@
                         </div>
 
                     </div>
-                    <div class="flex flex-col w-[20%] items-end">
+                    <!--
+                    <div class="hidden sm:flex flex-col w-[20%] items-end">
                         <div class="flex bg-green-100 w-16 h-16 md:w-12 md:h-12 xl:w-16 xl:h-16 rounded-full items-center">
                             <i class="fa fa-money text-[1.5rem] md:text-[1.4rem] xl:text-[1.5rem] text-green-500 mx-auto"></i>
                         </div>
                         <i class="flex text-green-700 fa fa-arrow-down"></i>
                     </div>
+                    -->
                 </div>
             </div>
 
---
+++ public/includes/partials/home/account-list.php
@@ -0,0 +1,142 @@
+<div class="flex flex-col mb-5 pt-10 md:pt-12 lg:pt-14 sm:flex-row sm:flex-wrap w-full content-between items-center rounded-2xl">
+    <div class="flex w-full ml-2 justify-center items-center">
+        <span class="flex"><i class="fa fa-wallet text-xl text-slate-500"></i></span>
+        <h1 class="flex font-sans text-lg sm:text-xl w-full mx-auto content-start px-3 py-4 text-slate-900">Tus cuentas</h1>
+    </div>
+    <div class="flex w-11/12 h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 py-4 px-6 rounded-2xl card-box-shadow card-box-shadow:hover bg-white"> <!-- card 1 -->
+        <div class="flex flex-col w-[80%]">
+            <p class="font-serif mb-1 text-md text-zinc-500">GOTECH</p>
+            <a href="#">
+                <h2 class="def-f-family font-medium text-2xl text-slate-700"><i class="fa fa-dollar-sign"></i> 164589,00</i></h2>
+            </a>
+            <div class="flex w-1/2 gap-2">
+                <div>
+                    <span class="text-zinc-500">Movs:</span>
+                </div>
+                <div class="flex content-center items-center">
+                    <span class="font-bold text-sm mr-1 text-green-700"><i class="fa fa-arrow-down"></i></span>
+                    <span class="text-sm text-zinc-500">2</span>
+                </div>
+                <div class="flex content-center items-center">
+                    <span class="font-bold text-sm mr-1 text-red-700"><i class="fa fa-arrow-up"></i></span>
+                    <span class="text-sm text-zinc-500">8</span>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col w-[20%]">
+            <div class="flex bg-emerald-100 w-16 h-16 md:w-12 md:h-12 xl:w-16 xl:h-16 rounded-full items-center">
+                <i class="fa fa-wallet text-[1.5rem] md:text-[1.4rem] xl:text-[1.5rem] text-lime-700 mx-auto"></i>
+            </div>
+        </div>
+    </div>
+    <div class="flex w-11/12 h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 py-4 px-6 rounded-2xl card-box-shadow card-box-shadow:hover bg-white"> <!-- card 2 -->
+        <div class="flex flex-col w-[80%]">
+            <p class="font-serif mb-1 text-md text-zinc-500">DELMA</p>
+            <a href="#">
+                <h2 class="def-f-family font-medium text-2xl  text-slate-700"><i class="fa fa-dollar-sign"></i> 14149,80</i></h2>
+            </a>
+            <div class="flex w-1/2 gap-2">
+                <div>
+                    <span class="text-zinc-500">Movs:</span>
+                </div>
+                <div class="flex content-center items-center">
+                    <span class="font-bold text-sm mr-1 text-green-700"><i class="fa fa-arrow-down"></i></span>
+                    <span class="text-sm text-zinc-500">105</span>
+                </div>
+                <div class="flex content-center items-center">
+                    <span class="font-bold text-sm mr-1 text-red-700"><i class="fa fa-arrow-up"></i></span>
+                    <span class="text-sm text-zinc-500">134</span>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col w-[20%]">
+            <div class="flex bg-emerald-100 w-16 h-16 md:w-12 md:h-12 xl:w-16 xl:h-16 rounded-full items-center">
+                <i class="fa fa-wallet text-[1.5rem] md:text-[1.4rem] xl:text-[1.5rem] text-lime-700 mx-auto"></i>
+            </div>
+        </div>
+    </div>
+    <div class="flex w-11/12 h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 py-4 px-6 rounded-2xl card-box-shadow card-box-shadow:hover bg-white"> <!-- card 3 -->
+        <div class="flex flex-col w-[80%]">
+            <p class="font-serif mb-1 text-md text-zinc-500">DESBLOQUEOS</p>
+            <a href="#">
+                <h2 class="def-f-family font-medium text-2xl  text-slate-700"><i class="fa fa-dollar-sign"></i> 4500,00</i></h2>
+            </a>
+            <div class="flex w-1/2 gap-2">
+                <div>
+                    <span class="text-zinc-500">Movs:</span>
+                </div>
+                <div class="flex content-center items-center">
+                    <span class="font-bold text-sm mr-1 text-green-700"><i class="fa fa-arrow-down"></i></span>
+                    <span class="text-sm text-zinc-500">4</span>
+                </div>
+                <div class="flex content-center items-center">
+                    <span class="font-bold text-sm mr-1 text-red-700"><i class="fa fa-arrow-up"></i></span>
+                    <span class="text-sm text-zinc-500">30</span>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col w-[20%]">
+            <div class="flex bg-yellow-100 w-16 h-16 md:w-12 md:h-12 xl:w-16 xl:h-16 rounded-full items-center">
+                <i class="fa fa-wallet text-[1.5rem] md:text-[1.4rem] xl:text-[1.5rem] text-yellow-500 mx-auto"></i>
+            </div>
+        </div>
+    </div>
+    <div class="flex w-11/12 h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 py-4 px-6 rounded-2xl card-box-shadow card-box-shadow:hover bg-white"> <!-- card 4 -->
+        <div class="flex flex-col w-[80%]">
+            <p class="font-serif mb-1 text-md text-zinc-500">IFE 5</p>
+            <a href="#">
+                <h2 class="def-f-family font-medium text-2xl text-slate-700"><i class="fa fa-dollar-sign"></i> 89,12</i></h2>
+            </a>
+            <div class="flex w-1/2 gap-2">
+                <div>
+                    <span class="text-zinc-500">Movs:</span>
+                </div>
+                <div class="flex content-center items-center">
+                    <span class="font-bold text-sm mr-1 text-green-700"><i class="fa fa-arrow-down"></i></span>
+                    <span class="text-sm text-zinc-500">1</span>
+                </div>
+                <div class="flex content-center items-center">
+                    <span class="font-bold text-sm mr-1 text-red-700"><i class="fa fa-arrow-up"></i></span>
+                    <span class="text-sm text-zinc-500">340</span>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col w-[20%]">
+            <div class="flex bg-red-100 w-16 h-16 md:w-12 md:h-12 xl:w-16 xl:h-16 rounded-full items-center">
+                <i class="fa fa-wallet text-[1.5rem] md:text-[1.4rem] xl:text-[1.5rem] text-red-400 mx-auto"></i>
+            </div>
+        </div>
+    </div>
+    <div class="flex w-11/12 h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 py-4 px-6 rounded-2xl card-box-shadow card-box-shadow:hover bg-white"> <!-- card 5 -->
+        <div class="flex flex-col w-[80%]">
+            <p class="font-serif mb-1 text-md text-zinc-500">EDU</p>
+            <a href="#">
+                <h2 class="def-f-family font-medium text-2xl  text-slate-700"><i class="fa fa-dollar-sign"></i> 7164349,18</i></h2>
+            </a>
+            <div class="flex w-1/2 gap-2">
+                <div>
+                    <span class="text-zinc-500">Movs:</span>
+                </div>
+                <div class="flex content-center items-center">
+                    <span class="font-bold text-sm mr-1 text-green-700"><i class="fa fa-arrow-down"></i></span>
+                    <span class="text-sm text-zinc-500">1200</span>
+                </div>
+                <div class="flex content-center items-center">
+                    <span class="font-bold text-sm mr-1 text-red-700"><i class="fa fa-arrow-up"></i></span>
+                    <span class="text-sm text-zinc-500">132340</span>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col w-[20%]">
+            <div class="flex bg-blue-100 w-16 h-16 md:w-12 md:h-12 xl:w-16 xl:h-16 rounded-full items-center">
+                <i class="fa fa-wallet text-[1.5rem] md:text-[1.4rem] xl:text-[1.5rem] text-blue-500 mx-auto"></i>
+            </div>
+        </div>
+    </div>
+    <!--
+    <div class="flex w-full h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 rounded-2xl bg-slate-400"></div>
+    <div class="flex w-full h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 rounded-2xl bg-slate-400"></div>
+    <div class="flex w-full h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 rounded-2xl bg-slate-400"></div>
+    <div class="flex w-full h-32 sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 rounded-2xl bg-slate-400"></div>  
+    -->        
+</div>
