c03d715c9a8341a6518773e4d9bcee3b818da2c2
coderEdu <educoder78@gmail.com>
2022-10-31 17:26:29 -0300

    set back-color cards / font size account list

--- public/includes/partials/home/account-list.php
+++ public/includes/partials/home/account-list.php
@@ -5,13 +5,13 @@
     </div>
     <div class="flex w-11/12 h-fit sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 py-4 px-6 rounded-2xl card-box-shadow card-box-shadow:hover bg-white"> <!-- card 1 -->
         <div class="flex flex-col w-[80%]">
-            <p class="font-serif mb-1 text-md text-zinc-500">GOTECH</p>
+            <p class="font-serif mb-1 text-sm text-zinc-500">GOTECH</p>
             <a href="#">
-                <h2 class="def-f-family font-medium text-2xl text-slate-700"><i class="fa fa-dollar-sign"></i> 164589,00</i></h2>
+                <h2 class="def-f-family font-medium text-xl text-slate-700"><i class="fa fa-dollar-sign"></i> 164589,00</i></h2>
             </a>
             <div class="flex w-1/2 gap-2">
                 <div>
-                    <span class="text-zinc-500">Movs:</span>
+                    <span class="text-sm text-zinc-500">Movs:</span>
                 </div>
                 <div class="flex content-center items-center">
                     <span class="font-bold text-sm mr-1 text-green-700"><i class="fa fa-arrow-down"></i></span>
@@ -31,13 +31,13 @@
     </div>
     <div class="flex w-11/12 h-fit sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 py-4 px-6 rounded-2xl card-box-shadow card-box-shadow:hover bg-white"> <!-- card 2 -->
         <div class="flex flex-col w-[80%]">
-            <p class="font-serif mb-1 text-md text-zinc-500">DELMA</p>
+            <p class="font-serif mb-1 text-sm text-zinc-500">DELMA</p>
             <a href="#">
-                <h2 class="def-f-family font-medium text-2xl  text-slate-700"><i class="fa fa-dollar-sign"></i> 14149,80</i></h2>
+                <h2 class="def-f-family font-medium text-xl text-slate-700"><i class="fa fa-dollar-sign"></i> 14149,80</i></h2>
             </a>
             <div class="flex w-1/2 gap-2">
                 <div>
-                    <span class="text-zinc-500">Movs:</span>
+                    <span class="text-sm text-zinc-500">Movs:</span>
                 </div>
                 <div class="flex content-center items-center">
                     <span class="font-bold text-sm mr-1 text-green-700"><i class="fa fa-arrow-down"></i></span>
@@ -57,13 +57,13 @@
     </div>
     <div class="flex w-11/12 h-fit sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 py-4 px-6 rounded-2xl card-box-shadow card-box-shadow:hover bg-white"> <!-- card 3 -->
         <div class="flex flex-col w-[80%]">
-            <p class="font-serif mb-1 text-md text-zinc-500">DESBLOQUEOS</p>
+            <p class="font-serif mb-1 text-sm text-zinc-500">DESBLOQUEOS</p>
             <a href="#">
-                <h2 class="def-f-family font-medium text-2xl  text-slate-700"><i class="fa fa-dollar-sign"></i> 4500,00</i></h2>
+                <h2 class="def-f-family font-medium text-xl text-slate-700"><i class="fa fa-dollar-sign"></i> 4500,00</i></h2>
             </a>
             <div class="flex w-1/2 gap-2">
                 <div>
-                    <span class="text-zinc-500">Movs:</span>
+                    <span class="text-sm text-zinc-500">Movs:</span>
                 </div>
                 <div class="flex content-center items-center">
                     <span class="font-bold text-sm mr-1 text-green-700"><i class="fa fa-arrow-down"></i></span>
@@ -83,13 +83,13 @@
     </div>
     <div class="flex w-11/12 h-fit sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 py-4 px-6 rounded-2xl card-box-shadow card-box-shadow:hover bg-white"> <!-- card 4 -->
         <div class="flex flex-col w-[80%]">
-            <p class="font-serif mb-1 text-md text-zinc-500">IFE 5</p>
+            <p class="font-serif mb-1 text-sm text-zinc-500">IFE 4</p>
             <a href="#">
-                <h2 class="def-f-family font-medium text-2xl text-slate-700"><i class="fa fa-dollar-sign"></i> 89,12</i></h2>
+                <h2 class="def-f-family font-medium text-xl text-slate-700"><i class="fa fa-dollar-sign"></i> 89,12</i></h2>
             </a>
             <div class="flex w-1/2 gap-2">
                 <div>
-                    <span class="text-zinc-500">Movs:</span>
+                    <span class="text-sm text-zinc-500">Movs:</span>
                 </div>
                 <div class="flex content-center items-center">
                     <span class="font-bold text-sm mr-1 text-green-700"><i class="fa fa-arrow-down"></i></span>
@@ -109,13 +109,13 @@
     </div>
     <div class="flex w-11/12 h-fit sm:w-[47.5%] md:w-[31.2%] lg:w-[23.4%] xl:w-[23.7%] 2xl:w-[23.9%] m-2 py-4 px-6 rounded-2xl card-box-shadow card-box-shadow:hover bg-white"> <!-- card 5 -->
         <div class="flex flex-col w-[80%]">
-            <p class="font-serif mb-1 text-md text-zinc-500">EDU</p>
+            <p class="font-serif mb-1 text-sm text-zinc-500">EDU</p>
             <a href="#">
-                <h2 class="def-f-family font-medium text-2xl  text-slate-700"><i class="fa fa-dollar-sign"></i> 7164349,18</i></h2>
+                <h2 class="def-f-family font-medium text-xl text-slate-700"><i class="fa fa-dollar-sign"></i> 7164349,18</i></h2>
             </a>
             <div class="flex w-1/2 gap-2">
                 <div>
-                    <span class="text-zinc-500">Movs:</span>
+                    <span class="text-sm text-zinc-500">Movs:</span>
                 </div>
                 <div class="flex content-center items-center">
                     <span class="font-bold text-sm mr-1 text-green-700"><i class="fa fa-arrow-down"></i></span>
