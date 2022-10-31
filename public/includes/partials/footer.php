6e1e8dbc2745c09aaad993c6fa4fcaf6c3b2af99
coderEdu <educoder78@gmail.com>
2022-10-31 00:38:57 -0300

    fix footer text-size and content

--- public/includes/partials/footer.php
+++ public/includes/partials/footer.php
@@ -1,5 +1,5 @@
 </body>
 <div class="flex w-full p-5 mt-10 bg-slate-50">
-    <footer class="mx-auto"><span>eduC. apps & sites -- web design -- &copy<?php echo date("Y"); ?></span></footer>
+    <footer class="text-sm sm:text-base mx-auto"><span>eduC &copy<?php echo date("Y"); ?>. Apps & Sites / web design.</span></footer>
 </div>
 </html>
