5f86d73fe6f169e39b4c1165a4bbc549520b4842
coderEdu <educoder78@gmail.com>
2022-11-05 19:17:38 -0300

    prog implem header-nav menu-items stage 2

---
+++ public/pages/notes-list.php
@@ -0,0 +1,13 @@
+<?php include "../partials/header/header.php" ?>
+<div class="container flex flex-col w-full h-screen mx-auto bg-default">
+    <?php include "../partials/header/header-nav.php"; ?>        
+    
+    <section>
+        <div class="w-full p-5 mx-auto">
+            <h1 class="font-sans font-normal text-2xl text-center text-blue-900">Todas tus notas.</h1>
+        </div>
+    </section>
+
+    <!-- footer -->
+    <?php include "../partials/footer/footer.php"; ?>
+</div>
--- public/partials/header/header-nav.php
+++ public/partials/header/header-nav.php
@@ -20,47 +20,13 @@
 
             <?php include "../partials/header-nav/menu/actions.php" ?>
 
-            <!-- 
-            <li x-data="{ open: false }">
-                <a href="#" @click="open = ! open">Acciones
-                    <ul>
-                        <div x-show="open" @click.outside="open = false" class="absolute ">
-                            <li><a href="#">Depositar</a></li>
-                            <li><a href="#">Extraer</a></li>
-                            <li><a href="#">Transferir</a></li>
-                        </div>
-                    </ul>
-                </a>
-            </li>
-            -->
-
             <?php include "../partials/header-nav/menu/notes.php" ?>
 
-            <!--
             <li>
-                <a href="#">Notas
-                    <ul class="hidden">
-                        <a href="#"><li>Crear</li></a>
-                        <a href="#"><li>Editar</li></a>
-                        <a href="#"><li>Eliminar</li></a>
-                        <a href="#"><li>Ver notas</li></a>
-                    </ul>
-                </a>
-            </li>
-            -->
-            <li>
-                <a href="log.php" class="flex items-center justify-center px-4 py-2 text-sm border rounded-full dark:border-neutral-700 focus:outline-none border-primary-500">Ver registro</a>
+                <a href="log.php" class="flex items-center justify-center px-4 py-2 text-sm border rounded-full dark:border-neutral-700 focus:outline-none border-primary-500 hover:bg-amber-500">Ver registro</a>
             </li>
         </ul>
     </nav>
-
-    <!-- 
-        <div x-data="{ open: false }">
-            <button @click="open = ! open">Toggle</button>
-        
-            <div x-show="open" @click.outside="open = false">Contents...</div>
-        </div>
-    -->
 
     <!-- burger icon -->
     <div class="flex px-6 md:hidden">
