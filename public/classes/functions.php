b07cdab9602869b92ab1c0afc6af2a20763715cb
coderEdu <educoder78@gmail.com>
2022-12-01 00:52:04 -0300

    fix logic of function colorBalance

--- public/classes/functions.php
+++ public/classes/functions.php
@@ -6,19 +6,19 @@ class MyFx
         $color=''; // you must initialize var to avoid function crash
         if (floatval($balance) > 100000) {
             $color = $value."-blue-500";
-        } else if (floatval($balance) > 50000) {
+        } else if (floatval($balance) >= 50000) {
             $color = $value."-blue-400";
-        } else if (floatval($balance) > 20000) {
+        } else if (floatval($balance) >= 20000) {
             $color = $value."-blue-300";
-        } else if (floatval($balance) > 10000) {
+        } else if (floatval($balance) >= 10000) {
             $color = $value."-yellow-400";
-        } else if (floatval($balance) > 5000) {
+        } else if (floatval($balance) >= 5000) {
             $color = $value."-orange-400";
-        } else if (floatval($balance) > 2500) {
+        } else if (floatval($balance) >= 2500) {
             $color = $value."-orange-500";
-        } else if (floatval($balance) > 1000) {
+        } else if (floatval($balance) >= 1000) {
             $color = $value."-red-400";
-        } else if (floatval($balance) > 500) {
+        } else if (floatval($balance) >= 500) {
             $color = $value."-red-500";
         } else if (floatval($balance) >= 0) {
             $color = $value."-red-600";
