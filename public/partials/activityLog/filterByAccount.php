9d60c41ecf315b99ce8502d0276c3eb4d66644c3
coderEdu <educoder78@gmail.com>
2022-12-19 16:07:40 -0300

    add empty option to account-select

--- public/partials/activityLog/filterByAccount.php
+++ public/partials/activityLog/filterByAccount.php
@@ -6,6 +6,7 @@
         <select id="account" class="px-2 py-2 w-full rounded-md" name="account"> <!-- working here -->
             <?php $id_user = $_SESSION['logged_id']; ?>
             
+            <option value=""></option>
             <?php foreach (MyQueries::getAccountsByLogged($conn,$id_user) as $row) { ?>
                 <option value="<?php echo $row['id']; ?>"><?php echo $row['nombre']; ?></option>
             <?php } ?>
