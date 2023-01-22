010fb8df4b5f97d7f3a70eafb3db02d721ed5e0d
coderEdu <educoder78@gmail.com>
2023-01-22 10:05:16 -0300

    little change

--- public/partials/activityLog/filterByAccount.php
+++ public/partials/activityLog/filterByAccount.php
@@ -1,8 +1,5 @@
 <div class="flex flex-row w-full justify-evenly items-center">
-    <div class="flex">
-        <label for="account" class="pr-2 font-sans font-normal text-base text-gray-500">En la cuenta:</label>
-    </div>
-    <div class="flex w-[70%]">
+    <div class="flex w-[50%]">
         <select id="account" class="px-2 py-2 w-full rounded-md" name="account"> <!-- working here -->
             <?php $id_user = $_SESSION['logged_id']; ?>
             
