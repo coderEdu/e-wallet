3efecbb554c631d756341593a194e8f0597a0a9b
coderEdu <educoder78@gmail.com>
2023-02-05 02:52:41 -0300

    Replace radio buttons with a select

--- public/partials/activityLog/filterByType.php
+++ public/partials/activityLog/filterByType.php
@@ -1,47 +1,11 @@
-<div class="flex flex-col space-y-5">
-    <div class="flex text-sm sm:text-base">
-        <input
-            id="dep"
-            name="type"
-            value="dep"
-            type="radio"
-            <?php if ($_GET['type'] == 'dep') { echo 'checked'; } ?>
-            class="w-6 h-6 bg-white rounded focus:ring-action-primary text-primary-500 border-primary border-neutral-500 dark:bg-neutral-700 dark:checked:bg-primary-500 focus:ring-primary-500"
-        />
-        <label for="dep" class="ml-3.5 flex flex-col flex-1 justify-center">
-            <span class="text-neutral-900 dark:text-neutral-100">Depósito</span>
-        </label>
-    </div>
-</div>
-
-<div class="flex flex-col space-y-5">
-    <div class="flex text-sm sm:text-base">
-        <input
-            id="ext"
-            name="type"
-            value="ext"
-            type="radio"
-            <?php if ($_GET['type'] == 'ext') { echo 'checked'; } ?>
-            class="w-6 h-6 bg-white rounded focus:ring-action-primary text-primary-500 border-primary border-neutral-500 dark:bg-neutral-700 dark:checked:bg-primary-500 focus:ring-primary-500"
-        />
-        <label for="ext" class="ml-3.5 flex flex-col flex-1 justify-center">
-            <span class="text-neutral-900 dark:text-neutral-100">Extracción</span>
-        </label>
-    </div>
-</div>
-
-<div class="flex flex-col space-y-5">
-    <div class="flex text-sm sm:text-base">
-        <input
-            id="tra"
-            name="type"
-            value="tra"
-            type="radio"
-            <?php if ($_GET['type'] == 'tra') { echo 'checked'; } ?>
-            class="w-6 h-6 bg-white rounded focus:ring-action-primary text-primary-500 border-primary border-neutral-500 dark:bg-neutral-700 dark:checked:bg-primary-500 focus:ring-primary-500"
-        />
-        <label for="tra" class="ml-3.5 flex flex-col flex-1 justify-center">
-            <span class="text-neutral-900 dark:text-neutral-100">Transferencia</span>
-        </label>
+<div class="flex flex-row w-full justify-evenly items-center">
+    <div class="flex w-[50%]">
+        <select id="type" class="px-2 py-2 w-full rounded-md" name="type"> <!-- working here -->
+            <?php $id_user = $_SESSION['logged_id']; ?>            
+            <option value=""></option>
+            <option value="dep" <?php if ($_GET['type'] == 'dep') { echo 'selected'; } ?> >Depósitos</option>
+            <option value="ext" <?php if ($_GET['type'] == 'ext') { echo 'selected'; } ?> >Extracciones</option>
+            <option value="tra" <?php if ($_GET['type'] == 'tra') { echo 'selected'; } ?> >Transferencias</option>
+        </select>
     </div>
 </div>
