63816755a18006c49d7f7c9b26ef619dfe586bc6
coderEdu <educoder78@gmail.com>
2023-02-12 22:18:54 -0300

    Change layout to menus

--- public/partials/header-nav/menu/actions.php
+++ public/partials/header-nav/menu/actions.php
@@ -1,6 +1,6 @@
 <div class="relative" x-data="{ open: false }" @mouseover="open = true" @mouseout="open = false">
     <button        
-        class="flex items-center justify-center px-4 py-2 text-sm border rounded-full dark:border-neutral-700 focus:outline-none border-primary-500 hover:text-slate-50 hover:bg-slate-700"
+        class="flex items-center justify-center px-4 py-2 text-sm border rounded-full focus:outline-none border-primary-500 hover:bg-amber-400 hover:text-slate-50"
         id="headlessui-popover-button-18"
         type="button"
         aria-expanded="true"
@@ -9,7 +9,7 @@
         <span>Movimientos</span><i class="ml-2 fa fa-angle-down"></i>
     </button>
     <div  x-show="open" x-transition class="absolute left-0 z-10 w-full max-w-sm px-0 mt-0 sm:px-0" id="headlessui-popover-panel-21">
-        <div class="overflow-hidden bg-slate-700 border shadow-xl rounded-2xl dark:bg-neutral-900 border-neutral-200 dark:border-neutral-700">
+        <div class="overflow-hidden bg-amber-400 border shadow-xl rounded-2xl border-neutral-200">
             <div class="relative w-full flex flex-col px-0 py-3 space-y-8">
                 <ul class="space-y-0 text-sm text-slate-50">
                     <li><a x-on:click="isNewDepOpen = true, open = false" href="#" class="block pl-4 hover:bg-slate-50 hover:text-black py-2">Depositar</a></li>
