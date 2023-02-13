5963ffe1fac2e0cb3c155728be95c2d88655dbf1
coderEdu <educoder78@gmail.com>
2023-02-13 01:28:32 -0300

    little change

--- public/partials/activityLog/filters.php
+++ public/partials/activityLog/filters.php
@@ -87,7 +87,7 @@
                     </div>
 
                     <!-- buttons -->
-                    <div class="flex items-center justify-end p-3 space-x-4 bg-neutral-50 dark:bg-neutral-900 dark:border-t dark:border-neutral-800">
+                    <div class="flex items-center justify-end p-3 space-x-4 bg-neutral-50">
                         <button
                             type="submit"
                             class="relative inline-flex items-center justify-center h-auto px-4 py-2 bg-cyan-700 text-xs text-white font-medium transition-colors rounded-full sm:text-base sm:px-5 ttnc-ButtonPrimary disabled:bg-opacity-70 bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-600 dark:focus:ring-offset-0"
