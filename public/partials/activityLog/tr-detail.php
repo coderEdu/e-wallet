354be71d6765b9e2d474d834ef17f1b39896e5a2
coderEdu <educoder78@gmail.com>
2023-02-18 07:23:29 -0300

    add increased/decreased account balance indicator

--- public/partials/activityLog/tr-detail.php
+++ public/partials/activityLog/tr-detail.php
@@ -6,7 +6,13 @@
 <div class="flex flex-row pt-1 justify-end items-center space-x-2">
     <div class="flex">
         <h2 class="def-f-family font-medium text-lg text-end sm:text-xl" style="color: <?php echo ($row['concepto']!='$correctivo') ? "#212F3D" : "#B6B6B6"; ?>;">
-            <i class="fa fa-dollar-sign text-base pr-1"></i><span class="def-f-family font-medium text-lg"><?php echo $row['monto']; ?></span>                   
+            <?php if ($row['tipo']=='rec') { ?>
+                <span class="text-base text-green-600">+ </span><i class="fa fa-dollar-sign text-base pr-1"></i><span class="def-f-family font-medium text-lg"><?php echo $row['monto']; ?></span> 
+            <?php } else if ($row['tipo']=='tra') { ?>    
+                <span class="text-xl font-normal text-red-600">- </span><i class="fa fa-dollar-sign text-base pr-1"></i><span class="def-f-family font-medium text-lg"><?php echo $row['monto']; ?></span>   
+            <?php } else { ?>   
+                <i class="fa fa-dollar-sign text-base pr-1"></i><span class="def-f-family font-medium text-lg"><?php echo $row['monto']; ?></span> 
+            <?php } ?>          
         </h2>
     </div>
     <div class="flex">
@@ -14,7 +20,7 @@
                 <i class="flex fa fa-arrow-up font-bold text-xs py-1 px-1 rounded-md text-white opacity-60" style="background-color: <?php echo ($row['concepto']!='$correctivo') ? "#E30000" : "#B6B6B6"; ?>;"></i>
         <?php } elseif ($row['tipo']=='dep') { ?>
                 <i class="flex fa fa-arrow-down font-bold text-xs py-1 px-1 rounded-md text-white opacity-50" style="background-color: <?php echo ($row['concepto']!='$correctivo') ? "#0CA002" : "#B6B6B6"; ?>;"></i>
-        <?php }  elseif ($row['tipo']=='tra') { ?>
+        <?php }  else { ?>
                 <i class="flex font-bold text-xs py-1 px-1 rounded-md text-white opacity-70 bg-yellow-500 fa fa-retweet"></i>
         <?php } ?>
     </div>
