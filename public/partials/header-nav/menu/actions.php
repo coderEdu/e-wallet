15ba6a29a31e16102a83b18e858f0249a3fa4cc8
coderEdu <educoder78@gmail.com>
2023-02-23 10:02:15 -0300

    Change menu style to white

--- public/partials/header-nav/menu/actions.php
+++ public/partials/header-nav/menu/actions.php
@@ -1,6 +1,6 @@
 <div class="relative" x-data="{ open: false }" @mouseover="open = true" @mouseout="open = false">
     <button        
-        class="flex items-center justify-center px-4 py-2 text-sm border rounded-full focus:outline-none border-primary-500 hover:bg-amber-400 hover:text-slate-50"
+        class="flex items-center justify-center px-4 py-2 text-sm border rounded-full focus:outline-none border-primary-500 hover:text-slate-800"
         id="headlessui-popover-button-18"
         type="button"
         aria-expanded="true"
@@ -9,14 +9,14 @@
         <span>Movimientos</span><i class="ml-2 fa fa-angle-down"></i>
     </button>
     <div  x-show="open" x-transition class="absolute left-0 z-10 w-full max-w-sm px-0 mt-0 sm:px-0" id="headlessui-popover-panel-21">
-        <div class="overflow-hidden bg-amber-400 border shadow-xl rounded-2xl border-neutral-200">
+        <div class="overflow-hidden bg-white border shadow-xl rounded-lg border-neutral-200">
             <div class="relative w-full flex flex-col px-0 py-3 space-y-8">
-                <ul class="space-y-0 text-sm text-slate-50">
-                    <li><a x-on:click="isNewDepOpen = true, open = false" href="#" class="block pl-4 hover:bg-slate-50 hover:text-black py-2">Depositar</a></li>
-                    <li><a x-on:click="isNewExtOpen = true, open = false" href="#" class="block pl-4 hover:bg-slate-50 hover:text-black py-2">Extraer</a></li>
-                    <li><a x-on:click="isNewTraOpen = true, open = false" href="#" class="block pl-4 hover:bg-slate-50 hover:text-black py-2">Transferir</a></li>
+                <ul class="space-y-0 text-sm text-slate-900">
+                    <li><a x-on:click="isNewDepOpen = true, open = false" href="#" class="block pl-4 hover:bg-zinc-100  py-2">Depositar</a></li>
+                    <li><a x-on:click="isNewExtOpen = true, open = false" href="#" class="block pl-4 hover:bg-zinc-100  py-2">Extraer</a></li>
+                    <li><a x-on:click="isNewTraOpen = true, open = false" href="#" class="block pl-4 hover:bg-zinc-100  py-2">Transferir</a></li>
                     <hr>
-                    <li><a href="log.php" class="block pl-4 hover:bg-slate-50 hover:text-black py-2">Ver registro</a></li>
+                    <li><a href="log.php" class="block pl-4 hover:bg-zinc-100  py-2">Ver registro</a></li>
                 </ul>
             </div>
         </div>
--- public/partials/header-nav/menu/sessions.php
+++ public/partials/header-nav/menu/sessions.php
@@ -1,6 +1,6 @@
 <div class="relative" x-data="{ open: false }" @mouseover="open = true" @mouseout="open = false">
     <button        
-        class="flex items-center justify-center px-4 py-2 text-sm border rounded-full focus:outline-none border-primary-500 hover:bg-amber-400 hover:text-slate-50"
+        class="flex items-center justify-center px-4 py-2 text-sm border rounded-full focus:outline-none border-primary-500 hover:text-slate-800"
         id="headlessui-popover-button-18"
         type="button"
         aria-expanded="true"
@@ -9,12 +9,12 @@
         <span></span><i class="fa fa-angle-down"></i>
     </button>
     <div  x-show="open" x-transition class="absolute left-0 z-10 w-[150px] max-w-sm px-0 mt-0 sm:px-0" id="headlessui-popover-panel-21">
-        <div class="overflow-hidden bg-amber-400 border shadow-xl rounded-2xl border-neutral-200">
+        <div class="overflow-hidden bg-white border shadow-xl rounded-lg border-neutral-200">
             <div class="relative flex flex-col px-0 py-3 space-y-8">
-                <ul class="space-y-0 text-sm text-slate-50">
-                    <li><a href="close-session.php" class="block pl-4 hover:bg-slate-50 hover:text-black py-2">Cerrar sesión</a></li>
-                    <li><a href="#" x-on:click="isChangePasswordOpen = true, open = false" class="block pl-4 hover:bg-slate-50 hover:text-black py-2">Cambiar clave</a></li>
-                    <li><a href="#" class="block pl-4 hover:bg-slate-50 hover:text-black py-2">Editar perfil</a></li>
+                <ul class="space-y-0 text-sm text-slate-900">
+                    <li><a href="close-session.php" class="block pl-4 hover:bg-zinc-100 py-2">Cerrar sesión</a></li>
+                    <li><a href="#" x-on:click="isChangePasswordOpen = true, open = false" class="block pl-4 hover:bg-zinc-100 py-2">Cambiar clave</a></li>
+                    <li><a href="#" class="block pl-4 hover:bg-zinc-100 py-2">Editar perfil</a></li>
                 </ul>
             </div>
         </div>
