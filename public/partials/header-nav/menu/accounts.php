2acb820ece37b897cdf4dc825e721ac14525b634
coderEdu <educoder78@gmail.com>
2023-03-02 10:30:16 -0300

    padding-top to menus

--- public/partials/header-nav/menu/accounts.php
+++ public/partials/header-nav/menu/accounts.php
@@ -8,7 +8,7 @@
     >
         <span>Cuentas</span><i class="ml-2 fa fa-angle-down"></i>
     </button>
-    <div  x-show="open" x-transition class="absolute left-0 z-10 w-full max-w-sm px-0 mt-0 sm:px-0" id="headlessui-popover-panel-21">
+    <div  x-show="open" x-transition class="absolute left-0 z-10 w-full max-w-sm pt-3 px-0 mt-0 sm:px-0" id="headlessui-popover-panel-21">
         <div class="overflow-hidden bg-white border shadow-xl rounded-lg border-neutral-200">
             <div class="relative w-full flex flex-col px-0 py-3 space-y-8">
                 <ul class="space-y-0 text-sm text-slate-900">
