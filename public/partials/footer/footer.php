a46828103b811570a7b7be54485ed33ce0ff7926
coderEdu <educoder78@gmail.com>
2023-03-08 11:30:48 -0300

    Activate menus based on accounts/movs existence

--- public/partials/footer/footer.php
+++ public/partials/footer/footer.php
@@ -1,6 +1,6 @@
     <footer class="flex w-full text-sm sm:text-base py-4 mt-auto bg-gray-50 text-neutral-500 rounded-xl">
         <div class="flex mx-auto">
-            <span>eduC &copy<?php echo date("Y"); ?>. Apps & Sites / web design.</span>
+            <span>eduC &copy<?php echo date("Y"); ?>. Apps & Sites / web design - Merlo. SL. Argentina</span>
         </div>
     </footer>
 </body>
--- public/partials/header-nav/menu/actions.php
+++ public/partials/header-nav/menu/actions.php
@@ -11,12 +11,28 @@
     <div  x-show="open" x-transition class="absolute left-0 z-10 w-full max-w-sm pt-3 px-0 mt-0 sm:px-0" id="headlessui-popover-panel-21">
         <div class="overflow-hidden bg-white border shadow-xl rounded-lg border-neutral-200">
             <div class="relative w-full flex flex-col px-0 py-3 space-y-8">
-                <ul class="space-y-0 text-sm text-slate-900">
-                    <li><a x-on:click="isNewDepOpen = true, open = false" href="#" class="block pl-4 hover:bg-zinc-100  py-2">Depositar</a></li>
-                    <li><a x-on:click="isNewExtOpen = true, open = false" href="#" class="block pl-4 hover:bg-zinc-100  py-2">Extraer</a></li>
-                    <li><a x-on:click="isNewTraOpen = true, open = false" href="#" class="block pl-4 hover:bg-zinc-100  py-2">Transferir</a></li>
-                    <hr>
-                    <li><a href="log.php" class="block pl-4 hover:bg-zinc-100  py-2">Ver registro</a></li>
+                <ul class="space-y-0 text-sm">
+                    <?php if ( $_SESSION['activate_menus_dep_ext'] ) { ?>
+                        <li><a x-on:click="isNewDepOpen = true, open = false" href="#" class="block pl-4 hover:bg-zinc-100 py-2 text-slate-900">Depositar</a></li>
+                        <li><a x-on:click="isNewExtOpen = true, open = false" href="#" class="block pl-4 hover:bg-zinc-100 py-2 text-slate-900">Extraer</a></li>
+                    <?php } else { ?>
+                        <li class="block pl-4 hover:bg-zinc-100 py-2 text-gray-300">Depositar</li>
+                        <li class="block pl-4 hover:bg-zinc-100 py-2 text-gray-300">Extraer</li>
+                    <?php } ?>
+
+                    <?php if ( $_SESSION['activate_menu_transfer'] ) { ?>
+                        <li><a x-on:click="isNewTraOpen = true, open = false" href="#" class="block pl-4 hover:bg-zinc-100 py-2 text-slate-900">Transferir</a></li>         
+                    <?php } else { ?>
+                        <li class="block pl-4 hover:bg-zinc-100 py-2 text-gray-300">Transferir</li>
+                    <?php } ?>
+
+                    <?php if ( $_SESSION['activate_menu_reg'] ) { ?>
+                        <hr>
+                        <li><a href="log.php" class="block pl-4 hover:bg-zinc-100 py-2 text-slate-900">Registro</a></li>         
+                    <?php } else { ?>
+                        <hr>
+                        <li class="block pl-4 hover:bg-zinc-100 py-2 text-gray-300">Registro</li>
+                    <?php } ?>
                 </ul>
             </div>
         </div>
