15ff949d22b91359ea314314d6d8895dc50b8ba1
coderEdu <educoder78@gmail.com>
2023-04-30 01:26:15 -0300

    The logic for updating notes is complete

--- public/pages/note-opened.php
+++ public/pages/note-opened.php
@@ -3,6 +3,17 @@ include_once "../partials/bd/conn.php";
 include_once "../queries/myQueries.php";
 include_once "../classes/functions.php";
 
+if (isset($_POST['edit_chkbox'])) {
+    $title = (isset($_POST['title'])) ? $_POST['title'] : '';
+    $note = (isset($_POST['note'])) ? $_POST['note'] : '';
+    $check = (isset($_POST['edit_chkbox'])) ? $_POST['edit_chkbox'] : '';
+    $id_user = intval($_SESSION['logged_id']);
+    $id_note = intval($_SESSION['noteId']);
+
+    $r = '';
+    if ($check=='on')
+        $r=MyQueries::updateNote($conn,$note,$id_user,$id_note);
+}
 ?>
 <?php
 // I set this session-var to false to stop viewing the form
@@ -37,45 +48,36 @@ $_SESSION['open-note']='false';
                 </div>
 
                 <div class="flex pt-4">
-                    <textarea class="border-2 py-2 px-2" name="note" id="note" cols="34" rows="8" readonly placeholder="Texto de la nota"><?php echo (isset($row['nota'])) ? $row['nota'] : ''; ?></textarea>
+                    <div class="flex flex-col">
+                        <textarea class="flex border-2 py-2 px-2" name="note" id="note" cols="34" rows="8" readonly placeholder="Texto de la nota"><?php echo (isset($row['nota'])) ? $row['nota'] : ''; ?></textarea>
+                        <div class="flex pt-1 space-x-1">
+                            <input type="checkbox" name="edit_chkbox" id="edit_chkbox" onclick="edit()">
+                            <label for="edit_chkbox">Editar</label>
+                        </div>
+                    </div>
                 </div>
                 
-            </form>
-            <div class="flex justify-between pt-4 items-center">
-                <div class="flex justify-between items-center space-x-2">
+                <div class="flex justify-end pt-4 items-center">
                     <div class="flex">
-                        <button class="border-3 bg-yellow-200 py-1 px-2" id="editBtn" onclick="edit()">
-                            Editar
-                            <!--<img src="../img/Actions-document-edit-icon.png" alt="edit-icon">-->
-                        </button>
-                    </div>
-                    <div class="flex">
-                        <button class="border-2 py-1 px-2" id="saveBtn" onclick="save()">
-                            Guardar
-                            <!--<img src="../img/Actions-document-close-icon.png" alt="delete-icon">-->
-                        </button>
-                    </div>
-                    <div>
-                        <span>
-                        <i class="fa-floppy-disk"></i>
-                        </span>
+                        <button type="submit" class="border-2 py-1 px-2" id="saveBtn" ">Guardar</button>
                     </div>
                 </div>
-            </div>
+            </form>
+
             <script>
                
-                let edit_button = document.getElementById('editBtn');
+                let edit_check = document.getElementById('edit_chkbox');
                 let save_button = document.getElementById('saveBtn');
                 let note = document.getElementById('note');
-                let note_content = note.value;
+                let note_save_content = note.value;
 
                 // Checks if the 'textarea' content has changed
                 note.addEventListener('input', () => {
-                    (note_content !== note.value) ? enablingSaveBtn() : disablingSaveBtn();
+                    (note_save_content !== note.value) ? enablingSaveBtn() : disablingSaveBtn();
                 });
                 
                 function load() {
-                    edit_button.style.borderColor='#FFF700';
+                    //edit_check.style.borderColor='#FFF700';
                     save_button.style.backgroundColor='#E7E7E7';
                     save_button.style.borderColor='#DFDFDF';
                     save_button.style.color='#C1C1C1';
@@ -89,6 +91,7 @@ $_SESSION['open-note']='false';
                     save_button.style.color='white';
                 }
 
+                
                 function disablingSaveBtn() {
                     save_button.style.backgroundColor='#E7E7E7';
                     save_button.style.borderColor='#DFDFDF';
@@ -98,26 +101,29 @@ $_SESSION['open-note']='false';
                 
                 function edit() {
                     //document.getElementById('note').setAttribute('readonly',true);
-                    edit_button.textContent='Editando ...'; 
-                    note.removeAttribute('readonly');
-                    note.style.backgroundColor='#FFFFD9';                    
-                    note.focus();
-                }
-
-                function save() {
-                    if (save_button.disabled !== true) {
+                    if (edit_check.checked) {
+                        if (note_save_content !== note.value)
+                            enablingSaveBtn();
+                        note.removeAttribute('readonly');
+                        note.style.backgroundColor='#FFFFD9';                    
+                        note.focus();
+                    } else {
                         note.style.backgroundColor='white';
                         note.setAttribute('readonly',true);
-                        edit_button.textContent='Editar';
                         disablingSaveBtn();
                     }
                 }
 
+                /*
+                function save() {
+                    note.style.backgroundColor='white';
+                    note.setAttribute('readonly',true);
+                    edit_check.checked=false;
+                    //edit_check.textContent='Editar';
+                    disablingSaveBtn();
+                }*/
+
             </script>
-            
-            <div class="flex pt-4">
-                <input type="hidden" name="">
-            </div>
         </div>
     </div>
 </div>
--- public/partials/home/last-notes-list/process-note.php
+++ public/partials/home/last-notes-list/process-note.php
@@ -1,9 +1,11 @@
 <?php
 session_start();
+$_SESSION['noteId'] = 0;
 if (isset($_GET['id'])) {
     $_SESSION['id_note'] = $_GET['id'];
     $_SESSION['open-note']='true';
     $_SESSION['wallets']='false';
 }
+$_SESSION['noteId'] = intval($_SESSION['id_note']);
 header("Location: ".$_SERVER['HTTP_REFERER']);
 ?>
