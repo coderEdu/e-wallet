b4f1a6c9588af42fdfbaf13c210c57023b6409d8
coderEdu <educoder78@gmail.com>
2023-05-22 10:42:35 -0300

    All notes layout is almost done

--- public/pages/notes-list.php
+++ public/pages/notes-list.php
@@ -8,16 +8,21 @@
             <h1 class="font-sans font-normal text-2xl text-center text-blue-900">Todas tus notas.</h1>
         </div>
         
-        <div class="grid sm:grid-cols-2 xl:grid-cols-3 mb-5 w-full justify-start items-center rounded-2xl">            
+        <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mb-5 w-full justify-start items-start rounded-2xl">            
             <?php
             $id = $_SESSION['logged_id'];
             foreach(MyQueries::getNotesByIdLogged($conn, $id) as $fila) { 
             ?>            
                 <div class="grid py-3 p-4 m-2 rounded-xl card-box-shadow card-box-shadow:hover bg-white"> <!-- begin card -->
-                    <div class="flex flex-col w-full max-h-56 space-y-1.5">                        
+                    <div class="flex flex-col w-full min-h-[200px] max-h-56 space-y-1.5">                        
                         <div class="relative flex justify-between items-start w-full rounded-tr-md">
                             <div class="flex">
-                                <p class="font-serif font-semibold text-xs md:text-base text-zinc-800"><?php echo ($fila[('titulo')]); ?></p>
+                                <?php 
+                                $str = $fila[('titulo')];
+                                if (strlen($str) > 16)
+                                    $str = substr($str, 0, 13) . '...';
+                                ?>
+                                <p class="font-serif font-semibold text-xs md:text-sm xl:text-base text-zinc-800"><?php echo $str; ?></p>
                             </div>
                             <div class="flex justify-end items-center w-12 h-3 sm:w-14 sm:h-4 space-x-1">
                                 <a href="">
@@ -34,7 +39,7 @@
                         </div>
 
                         <!-- note content -->
-                        <p class="w-full def-f-family font-light text-xs sm:text-sm lg:text-base py-2 px-2 overflow-y-auto text-slate-700 bg-white border-2 border-slate-100 rounded"><?php echo ($fila['nota']); ?></p>
+                        <p class="w-full def-f-family font-light text-xs sm:text-sm lg:text-sm xl:text-base py-2 px-2 min-h-[168px] overflow-y-auto text-slate-700 bg-white border-2 border-slate-100 rounded"><?php echo ($fila['nota']); ?></p>
 
                         <div class="flex w-full justify-between items-center font-light text-xs lg:text-sm font-serif text-neutral-700">
                             <div class="flex w-full">
